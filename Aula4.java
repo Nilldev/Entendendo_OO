5d4cdf5a2eae68a0ec3c4614f9d9e909f68731eb
NillDev <68431145+Nilldev@users.noreply.github.com>
2022-10-30 23:20:16 -0300

    metodos de acesso e modificadores

---
+++ Aula4.java
@@ -0,0 +1,13 @@
+public class Aula4 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Caneta4 c1 = new Caneta4();
+        c1.setModelo("BIC");
+        c1.setPonta(0.5f);
+        c1.status();
+    }
+    
+}
