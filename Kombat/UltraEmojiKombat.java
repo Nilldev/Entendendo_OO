aa5b92d121289fd36b79f10ac1721c473db957f6
NillDev <68431145+Nilldev@users.noreply.github.com>
2022-11-10 13:03:53 -0300

    Kombat

---
+++ Kombat/UltraEmojiKombat.java
@@ -0,0 +1,18 @@
+public class UltraEmojiKombat {
+    
+    public static void main(String[] args) {
+         Lutador L[] = new Lutador [3];
+         L[0] = new Lutador ("Blanka", "Brasil", 33, 1.75f, 80.9f, 15, 5, 2);
+    
+         L[1] = new Lutador ("Dalsin", "Índia", 45, 1.80f, 90.0f, 20, 8, 4 );
+
+         L[2] = new Lutador ("Ryu", "Japão", 29, 1.87f , 100.0f, 25, 1, 1 );
+
+         L[1].status();
+         
+
+
+        
+    }
+    
+}
